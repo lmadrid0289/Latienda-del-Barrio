ebb2fa9c64e9a15f3fe2f9228d0780d1a4caca8e
luis <lmadrid0289@gmail.com>
2021-01-16 13:23:54 -0500

    added redux actions shopping cart components and screens, change styles

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,7 +8,9 @@ import Product from '../models/productModel.js'
 //@access Public  
 router.get('/', 
 AsyncHandler( async (req,res)=>{
+
     const products = await  Product.find({})
+    
     res.json(products)
 })) 
 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import { Row, Col } from 'react-bootstrap'
-import axios from 'axios'
 import { listProducts } from '../actions/productActions'
 
 
@@ -18,7 +19,8 @@ const HomeScreen = () => {
     return (
         <>
             <h1>Latest Products</h1>
-            {loading ? <h2>Loading..</h2> : error ? <h3>{error}</h3> : <Row>
+            {loading ? (<Loader/> 
+            ): error ?( <Message variant='danger'>{error}</Message> ): <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                         <Product product={product} />
